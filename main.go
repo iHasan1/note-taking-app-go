beb59fec6e00ca3741941e85a631133757894a8e
Imran Hasan <imran.h94@gmail.com>
2024-02-07 11:31:17 +0500

    fixed the user input issue

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 import "example.com/note-taking-app/note"
+import "bufio"
+import "os"
+import "strings"
 
 func main() {
 	title, content := getNoteData()
@@ -27,8 +30,23 @@ func getNoteData() (string, string) {
 
 func getUserInput(prompt string) (string) {
 	fmt.Print(prompt)
-	var value string
-	fmt.Scanln(&value) // scanln also allows to register a enter key event
 
-	return value
+	/* scanning like this wont work with longer inputs
+	// var value string
+	// fmt.Scanln(&value) // scanln also allows to register a enter key event
+	*/
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')	// we need single quotes here, not double to specify single characters
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")	// we need to remove the line break from string because readString does not remove it
+	text = strings.TrimSuffix(text, "\r")	// in windows sometimes line break is defined by \r
+
+
+
+	return text
 }
